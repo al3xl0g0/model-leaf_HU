1ca82148e1cbfd024da4392bc59234b1ad6ad784
al3xl0g0 <44162435+al3xl0g0@users.noreply.github.com>
2020-04-27 02:18:32 +0300

    Full test depressionAng

--- DepressionAng.py
+++ DepressionAng.py
@@ -1,13 +1,12 @@
 from math import *
 import numpy as np
 
-
-# % depAng = sin-1((H^2 + 2HRe + R^2)/(2R(H + Re)))
+#depAng = sin-1((H^2 + 2HRe + R^2)/(2R(H + Re)))
 def asind(x):
     rad = np.arcsin(x)
     return np.degrees(rad)
 
-def depAng(H, R, Re=8.4774e+06): #changed from 4/3
+def depAng(H, R, Re=8.4774e+06): #changed from 4/3 radius of Earth
     H2 = np.multiply(2, H)
     H2Re = np.multiply(H2, Re)
     R2 = np.multiply(2, R)
