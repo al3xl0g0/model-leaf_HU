e6f21ad898dd1180002b6cd02b8a275948185d9f
al3xl0g0 <44162435+al3xl0g0@users.noreply.github.com>
2020-08-06 00:22:16 +0300

    Update Main Method

--- BRDFCorrectionMatlab_v1.py
+++ BRDFCorrectionMatlab_v1.py
@@ -33,7 +33,6 @@ MS730 = np.multiply(MS730_DN, DN2RAD[5])
 MS780 = np.multiply(MS780_DN, DN2RAD[6])
 
 # Convert Multi Data image into 3D point cloud
-# With dataToCloud_AgriEye function
 # Saving the Multi data value
 
 pcloudMS480 = dataToCloud(RGB, MS480, 0)
